c4d7c91619e4c1a70dc36ee0fe2cf0aa97b00ba4
auturnn <acki@kakao.com>
2020-11-05 05:56:31 +0900

    poster down Done

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -15,102 +16,133 @@ import (
 )
 
 type ParseHTML struct {
-	url      string
-	fileName string
-	downPath string
+	url      string // https://www.megabox.co.kr/movie-detail?rpstMovieNo=
+	respName string // 20046500
+	htmlPath string
+	htmlName string
+	htmlFile *os.File
+	imgPath  string
+	imgName  string
+	sqlPath  string
+	sqlName  string
+	sqlFile  *os.File
+
+	doc *goquery.Document
+
+	MovTest
+}
+
+//MovTest table structs
+type MovTest struct {
+	MovTitle []string
+	MovOpd   []string
+	MovType  []string
+	MovGnr   []string
+	MovAge   []string
+	MovPst   []string
+	MovDrt   []string
+	MovRnt   []string
+	MovPfm   []string
+	MovSmr   []string
+}
+
+type Megabox struct {
+	ImgPath   string
+	ImgSvrUrl string      `json:"imgSvrUrl"`
+	MovieList []MovieList `json:"movieList"`
+}
+
+type MovieList struct {
+	MovieNo   string `json:"movieNo"`
+	MovieNm   string `json:"movieNm"`
+	ImgPathNm string `json:"imgPathNm"`
 }
 
 func main() {
 	log.Println("main log...")
-	p := &ParseHTML{
-		url:      "https://www.megabox.co.kr/movie/comingsoon",
-		fileName: "./ing.html",
-		downPath: "./img/ing/",
+
+	m := &Megabox{
+		ImgPath: "./img/poster/ing/",
 	}
-	// p.HTMLDown()
-	p.PostDown()
+	m.ListData()
+
+	var wait sync.WaitGroup
+	wait.Add(len(m.MovieList))
+
+	for i, items := range m.MovieList {
+		log.Println(items)
+		go func(i int, items MovieList) {
+			defer wait.Done()
+			m.PosterDown(i, items)
+		}(i, items)
+	}
+	wait.Wait()
 }
 
-func (p *ParseHTML) HTMLDown() {
-	res, err := http.Get(p.url)
-	if err != nil {
-		log.Fatal(err)
-	}
+func (m *Megabox) ListData() {
+	url := "https://www.megabox.co.kr/on/oh/oha/Movie/selectMovieList.do"
+	method := "POST"
 
+	payload := strings.NewReader(`{
+	"currentPage": "1",
+	"ibxMovieNmSearch": "",
+	"onairYn": "Y",
+	"pageType": "ticketing",
+	"recordCountPerPage": "40",
+	"specialType": ""
+	}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Cookie", "WMONID=zcT2Xq9q57s; SameSite=None; SESSION=Yzc4OTJjYmYtNmU5NS00MGVhLWFmNDMtY2FkMWZkYjc3MjE1; JSESSIONID=oN4vAfK8XnaOkcCoDOZ1jxIY6CP27dVObnvHlnNx9IZomHQSVJWIWwQbaWDVIh3y.b25fbWVnYWJveF9kb21haW4vbWVnYS1vbi1zZXJ2ZXI1")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	defer res.Body.Close()
 
-	if res.StatusCode != 200 {
-		log.Fatalf("status code: %d %s", res.StatusCode, res.Status)
-	}
-
-	f1, err := os.Create(p.fileName)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		f1.Close()
-		log.Fatal("파일 생성 실패", err)
+		fmt.Println(err)
+		return
 	}
 
-	doc := htmlParse(res.Body)
-	html, _ := doc.Html()
-
-	_, err = f1.WriteString(html)
+	err = json.Unmarshal(body, m)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	f1.Close()
-
+	return
 }
 
-func (p *ParseHTML) PostDown() {
-	f1, err := ioutil.ReadFile(p.fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	rhtml := strings.NewReader(string(f1))
-	doc := htmlParse(rhtml)
-
-	log.Println("영화 카운트: ", doc.Find("div.movie-list").Find("ol").Find("li").Length())
+func (m *Megabox) PosterDown(i int, list MovieList) {
 	client := grab.NewClient()
+	request, _ := grab.NewRequest(m.ImgPath, m.ImgSvrUrl+list.ImgPathNm)
+	response := client.Do(request)
+	filename := response.Filename
 
-	postFind := doc.Find("div.movie-list").Find("ol").Find("li")
-	postFind.Each(func(i int, s *goquery.Selection) {
-		imgPath, _ := s.Find("img").Attr("src")
-		title := s.Find("div.tit-area").Find("p.tit").Text()
-		log.Println("title: " + title)
+	t := time.NewTicker(time.Second)
 
-		request, _ := grab.NewRequest(p.downPath, imgPath)
-		response := client.Do(request)
-		filename := response.Filename
-		log.Println(filename)
-
-		t := time.NewTicker(time.Second)
-
-		for {
-			select {
-			case <-t.C:
-				fmt.Printf("%.02f%% complete\n", response.Progress())
-
-			case <-response.Done:
-				err := response.Err()
-				if err != nil {
-					// ...
-				}
-				err = os.Rename(filename, p.downPath+title+".jpg")
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				log.Println(imgPath, response.HTTPResponse.StatusCode)
-				return
+	for {
+		select {
+		case <-t.C:
+			fmt.Printf("%.02f%% complete\n", response.Progress())
+		case <-response.Done:
+			err := response.Err()
+			if err != nil {
+				// ...
 			}
+			os.Rename(filename, m.ImgPath+list.MovieNm+".jpg")
+			log.Println(i, "번째 Done!"+m.ImgPath+list.MovieNm+".jpg", response.HTTPResponse.StatusCode)
+			return
 		}
-	})
-}
-
-func htmlParse(rhtml io.Reader) *goquery.Document {
-	doc, err := goquery.NewDocumentFromReader(rhtml)
-	if err != nil {
-		panic(err)
 	}
-	return doc
 }
