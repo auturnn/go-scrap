d6def67c8432d4c958a1ba415a9d72f16fb2d590
auturnn <acki@kakao.com>
2020-12-03 06:03:50 +0900

    modify type_mst sql

--- run/chart.go
+++ run/chart.go
@@ -49,11 +49,28 @@ func (m *Megabox) GetList() {
 	)
 	if m.SQLFileName == "./sql/movie-ing.sql" {
 		_, err = m.SQLFile.WriteString(
-			"DELETE FROM mov_type;\n" +
+			"DELETE FROM user_test;\n" +
+				"DELETE FROM reserv_mst;\n" +
+				"DELETE FROM rest_cnt;\n" +
+				"DELETE FROM play_mst;\n" +
+				"DELETE FROM seat_mst;\n" +
+				"DELETE FROM room_mst;\n" +
+				"DELETE FROM thea_mst;\n" +
+				"DELETE FROM city_mst;\n" +
+				"DELETE FROM mov_type;\n" +
+				"DELETE FROM type_mst;\n" +
 				"DELETE FROM mov_genre;\n" +
 				"DELETE FROM mov_img;\n" +
 				"DELETE FROM mov_dt;\n" +
 				"DELETE FROM mov_mst;\n" +
+				"ALTER TABLE type_mst AUTO_INCREMENT = 1;\n" +
+				"ALTER TABLE play_mst AUTO_INCREMENT = 1;\n" +
+				"ALTER TABLE city_mst AUTO_INCREMENT = 1;\n" +
+				"ALTER TABLE reserv_mst AUTO_INCREMENT = 1;\n" +
+				"ALTER TABLE rest_cnt AUTO_INCREMENT = 1;\n" +
+				"ALTER TABLE seat_mst AUTO_INCREMENT = 1;\n" +
+				"ALTER TABLE room_mst AUTO_INCREMENT = 1;\n" +
+				"ALTER TABLE thea_mst AUTO_INCREMENT = 1;\n" +
 				"ALTER TABLE mov_mst AUTO_INCREMENT = 1;\n",
 		)
 	}
@@ -77,13 +94,13 @@ func (m *Megabox) GetList() {
 
 			//정상적인 이미지 경로의 경우 모두 길이가 58로 고정.
 			if len(items.ImgPath) == 58 {
-				ext := items.ImgPath[len(items.ImgPath)-4:]
+				ext := items.ImgPath[len(items.ImgPath)-4:] //이미지마다 확장자가 다르기 때문에 ex) .jpg / .gif / .png
 				items.ImgName = m.ImgPath + items.Title + ext
 				m.PosterDown(items)
 				m.DetailRequest(&items)
 				m.CreateSQL(&items)
 			} else {
-				log.Println("Pass :", items.Title)
+				log.Println("[PASS] IMG ERROR :", items.Title)
 			}
 		}(items)
 	}
--- run/createSql.go
+++ run/createSql.go
@@ -28,48 +28,47 @@ func (m *Megabox) CreateSQL(items *MovieList) {
 		"\n\nINSERT INTO mov_mst(title, rank, cnt, open_date, stat, age)\n" +
 			"VALUES('" + items.Title + "', " + rank + ", " + cnt + ", '" + items.OpenDate + "', '" + items.Stat + "', '" + items.Age + "')\n" +
 			"ON DUPLICATE KEY UPDATE rank = " + rank + ", cnt =" + cnt + ", stat = '" + items.Stat + "', mov_idx = LAST_INSERT_ID(mov_idx);\n" +
-
+			"SET @movidx = LAST_INSERT_ID();\n" +
 			"INSERT INTO mov_dt " +
-			"VALUES((SELECT LAST_INSERT_ID()), '" +
+			"VALUES (@movidx, '" +
 			items.Direct + "', '" + items.Actor + "', '" + items.Runtime + "', '" + items.Summary + "')\n" +
-			"ON DUPLICATE KEY UPDATE\nmov_idx = LAST_INSERT_ID(mov_idx), director= '" +
-			items.Direct + "', actor='" + items.Actor + "', runtime='" + items.Runtime + "', " +
-			"summary= '" + items.Summary + "';\n" +
+			"ON DUPLICATE KEY UPDATE\n director= '" +
+			items.Direct + "', actor ='" + items.Actor + "', runtime ='" + items.Runtime + "', " +
+			"summary = '" + items.Summary + "';\n" +
 
 			func() string { // 장르는 따로 구분하여 넣기 때문에 슬라이싱하여 '장르의 수 만큼 구문을 생성'한다.
 				s := strings.Split(items.GenreName, ",")
 				gnr := ""
 				for _, s := range s {
 					s = strings.TrimSpace(s)
-					gnr += "INSERT INTO mov_genre(mov_idx,genre_name) SELECT (SELECT LAST_INSERT_ID()),'" + s +
+					gnr += "INSERT INTO mov_genre(mov_idx, genre_name) SELECT @movidx, '" + s +
 						"'\nFROM DUAL WHERE NOT EXISTS(SELECT mov_idx, genre_name FROM mov_genre\n" +
-						"WHERE mov_idx = (SELECT LAST_INSERT_ID()) AND genre_name='" + s + "');\n"
+						"WHERE mov_idx = @movidx AND genre_name='" + s + "');\n"
 				}
 				return gnr
 			}() +
 
+			"INSERT INTO mov_img(mov_idx,img_path)\n" +
+			"SELECT @movidx, '" + items.ImgName[1:] +
+			"'\nFROM DUAL WHERE NOT EXISTS(SELECT mov_idx, img_path FROM mov_img\n" +
+			"WHERE mov_idx = @movidx AND img_path='" + items.ImgName[1:] + "');\n" +
+
 			func() string { // 상영 타입 또한 여러가지 타입이 동시에 존재할 수 있기에 익명함수를 통해 '타입갯수만큼 생성' 후 리턴
 				s := strings.Split(items.TypeName, ",")
 				typ := ""
-				mtyp := ""
+
 				for _, s := range s {
 					s = strings.TrimSpace(s)
-
-					typ += "INSERT INTO type_mst(type_name) SELECT ('" + s +
-						"' FROM DUAL WHERE NOT EXISTS(SELECT type_name FROM type_mst " +
-						"WHERE type_name = '" + s + "');\n"
-
-					mtyp += "INSERT INTO mov_type(mov_idx,type_idx) SELECT (SELECT LAST_INSERT_ID()),'" + s +
-						"' FROM DUAL WHERE NOT EXISTS(SELECT mov_idx, type_idx FROM mov_type " +
-						"WHERE mov_idx = (SELECT LAST_INSERT_ID()) AND type_idx='" + s + "');\n"
+					typ += "INSERT INTO type_mst(type_name) " +
+						"VALUES('" + s + "')\n" +
+						"ON DUPLICATE KEY UPDATE\ntype_idx = (@typeidx := LAST_INSERT_ID(type_idx));\n" +
+						"INSERT INTO mov_type(mov_idx,type_idx)\n" +
+						"SELECT @movidx, @typeidx\n" +
+						"FROM DUAL WHERE NOT EXISTS(SELECT mov_idx, type_idx FROM mov_type\n" +
+						"WHERE mov_idx = @movidx AND type_idx=@typeidx);\n"
 				}
-				return typ + mtyp
-			}() +
-
-			"INSERT INTO MOV_IMG(mov_idx,MIMG_PATH)\n" +
-			"SELECT (SELECT LAST_INSERT_ID()), '" + items.ImgName[1:] +
-			"'\nFROM DUAL WHERE NOT EXISTS(SELECT mov_idx, MIMG_PATH FROM MOV_IMG\n" +
-			"WHERE mov_idx = (SELECT LAST_INSERT_ID()) AND MIMG_PATH='" + items.ImgName[1:] + "');\n",
+				return typ
+			}(),
 	)
 
 	if err != nil {
